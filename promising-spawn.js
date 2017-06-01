7a723f7aa7d5f828ad24c2de9e94763e7af5c1ff
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-06-01 22:39:14 +0200

    Adjust promising-spawn.js a bit
    I have a slightly different coding style.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- promising-spawn.js
+++ promising-spawn.js
@@ -15,63 +15,54 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- * The code is originally from * https://github.com/raineorshine/spawn-please.
+ * The code is originally from https://github.com/raineorshine/spawn-please.
  */
-var spawn = require('child_process').spawn
+var originalSpawn = require('child_process').spawn
 
-function spawnPlease(command, args, stdin, options) {
+if (typeof Promise === 'undefined')
+  throw new Error('Need Promises');
 
-  // if there are only three arguments and the third argument is an object, treat it as the options object and set stdin to null
-  if(!options && typeof stdin === 'object') {
-    options = stdin
-    stdin = undefined
+var spawn = function(command, args, stdin, options) {
+
+  // make stdin optional (options is an object, stdin is a string)
+  if (options === undefined && typeof stdin !== 'object') {
+    options = stdin;
+    stdin = undefined;
   }
 
   // defaults
-  options = options || {}
-  if(options.rejectOnError === undefined) {
-    options.rejectOnError = true
-  }
+  options = options || {};
+  if (options.rejectOnError === undefined)
+    options.rejectOnError = true;
 
-  var stdout = ''
-  var stderr = ''
-  var child = spawn(command, args, options)
+  var stdout = '';
+  var stderr = '';
+  var child = originalSpawn(command, args, options);
 
-  if(!spawnPlease.Promise) {
-    throw new Error('No built-in Promise. You will need to use a Promise library and spawnPlease.Promise = Promise.')
-  }
+  return new Promise(function (resolve, reject) {
 
-  return new spawnPlease.Promise(function (resolve, reject) {
-
-    if(stdin !== undefined) {
-      child.stdin.write(stdin)
-    }
-    child.stdin.end()
+    if (stdin !== undefined)
+      child.stdin.write(stdin);
+    child.stdin.end();
 
     child.stdout.on('data', function (data) {
-      stdout += data
-    })
+      stdout += data;
+    });
 
     child.stderr.on('data', function (data) {
-      stderr += data
-    })
+      stderr += data;
+    });
 
-    if(options.rejectOnError) {
-      child.addListener('error', reject)
-    }
+    if (options.rejectOnError)
+      child.addListener('error', reject);
 
     child.on('close', function (code) {
-      if(code !== 0 && options.rejectOnError) {
-        reject(stderr)
-      }
-      else {
-        resolve(stdout)
-      }
-    })
+      if (code !== 0 && options.rejectOnError)
+        reject(stderr);
+      else
+        resolve(stdout);
+    });
+  });
+};
 
-  })
-}
-
-spawnPlease.Promise = typeof Promise !== 'undefined' ? Promise : null
-
-module.exports = spawnPlease
+module.exports.spawn = spawn;
