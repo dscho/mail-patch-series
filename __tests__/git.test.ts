3796603a9aee3816422d263ef0e67e67b7a40200
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-05-08 09:12:54 -0400

    Start testing the new classes
    This is the most important aspect about the conversion of the script
    into proper, Object-Oriented code: to add tests, to be able to deploy
    with confidence.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- __tests__/git.test.ts
+++ __tests__/git.test.ts
@@ -1,5 +1,5 @@
 import 'jest';
-import { gitConfig } from './git';
+import { gitConfig } from '../lib/git';
 
 test('finds core.bare', async () => {
     expect(await gitConfig('core.bare')).toMatch(/true|false/);
